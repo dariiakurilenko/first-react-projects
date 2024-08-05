eb32c0983eb4c5539ce74c03ff6c9c3556559036
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-08-05 09:44:23 +0300

    uploaded my-first-react-project

---
+++ Project1-react/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function Navbar(){
+    return(
+        <nav>
+            <div className = 'nav-container'>
+                <img src='images/Group.png' className='nav-icon'/>
+                <h3 className='nav-text'>RealFacts</h3>
+                <h4 className='nav-description'>React Course - Project 1</h4>
+            </div>
+        </nav>
+    )
+}
